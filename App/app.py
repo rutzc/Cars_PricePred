3f4cf619d94bbbfe9115767dd64f532fc05b414a
rutzc <147026411+rutzc@users.noreply.github.com>
2024-05-16 17:47:50 +0200

    App - EDA

--- App/app.py
+++ App/app.py
@@ -10,6 +10,8 @@ Created on Thu May 16 2024
 
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 ###### Config ######
@@ -27,6 +29,12 @@ st.set_page_config(
 def load_data():
     data = pd.read_csv('./Data/sample_data_100k.csv')
     return data
+
+
+
+###### Daten laden ######
+data = load_data()
+
 
 
 ###### Sidebar ######
@@ -47,5 +55,71 @@ st.markdown("Finde mehr über den Wiederverkaufswert deines Autos heraus.") #Tex
 tab1, tab2 = st.tabs(["Explorative Datenanalyse", "Wiederverkaufswert-Rechner"]) #Tabs
 
 
-
 ###### Tab 1 - Explorative Datenanalyse ######
+
+with tab1:
+    #Subheader
+    st.header("Erkunde hier den Einfluss verschiedener Variablen auf den Preis", dvider = True)
+    
+    #Selection für x-Variable
+    selected_variable = st.selectbox("Wähle eine Variable", list(data.drop("price").columns))
+    
+    #Tickbox für Anzeige Raw Data
+    if st.checkbox("Rohdatensatz anzeigen", False):
+        st.subheader("Rohdaten")
+        st.write(data)
+    
+    #Numerische und kategorielle Variablen trennen
+    numeric_variables = ["age","average_fuel_economy", "horsepower", "mileage"]
+    categorical_variables = ["body_type", "engine_type", "fuel_type", "make_name", "model_name", "transmission", "wheel_system_display"]
+    
+    #Numerische Analyse
+    if selected_variable in numeric_variables:
+        st.subheader(f"Analyse numerischer Variable {selected_variable}")
+        
+        #Zwei Spalten
+        col1, col2 = st.columns([1, 1])
+        
+        #Histogramm
+        col1.write(f"Histogramm von {selected_variable}")
+        fig1, ax1 = plt.subplots(figsize=(8,3.7))
+        ax1.hist(data[selected_variable], color = "#fc8d62")
+        ax1.set_xlabel(selected_variable)
+        ax1.set_ylabel("Häufigkeit")
+        col1.pyplot(fig1, use_container_width=True)
+        
+        #Scatterplot mit Preis
+        col2.write(f"Scatterplot Preis vs. {selected_variable}")
+        fig2, ax2 = plt.subplots(figsize=(8, 3.7))
+        sns.regplot(x=selected_variable, y="price", data=data, ax=ax2, scatter_kws={'color': '#66c2a5'}, line_kws={'color': '#fc8d62'})
+        ax2.set_xlabel(selected_variable)
+        ax2.set_ylabel("Preis")
+        col2.pyplot(fig2, use_container_width=True)
+        
+        
+##weiter hier:
+        st.subheader("Kategorische Variablen")
+    categorical_columns = ['model', 'brand']  # Beispielhafte kategorische Variablen
+    selected_categorical = st.selectbox("Wähle eine kategorische Variable", categorical_columns)
+
+    if selected_categorical:
+        st.write(f"Boxplot von {selected_categorical} gegen Preis")
+        fig4, ax4 = plt.subplots(figsize=(8, 3.7))
+        sns.boxplot(x=selected_categorical, y='price', data=data, ax=ax4, palette="Set3")
+        plt.xticks(rotation=90)
+        ax4.set_xlabel(selected_categorical)
+        ax4.set_ylabel('Preis')
+        st.pyplot(fig4, use_container_width=True)
+
+        st.write(f"Barplot von {selected_categorical} gegen Preis")
+        fig5, ax5 = plt.subplots(figsize=(8, 3.7))
+        avg_price_by_category = data.groupby(selected_categorical)['price'].mean().reset_index()
+        sns.barplot(x=selected_categorical, y='price', data=avg_price_by_category, ax=ax5, palette="Set3")
+        plt.xticks(rotation=90)
+        ax5.set_xlabel(selected_categorical)
+        ax5.set_ylabel('Durchschnittspreis')
+        st.pyplot(fig5, use_container_width=True)
+
+
+
+
