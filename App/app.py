d379fd1d5fa460f2a6233abfb5a0bb4adb7f8621
rutzc <147026411+rutzc@users.noreply.github.com>
2024-05-20 13:03:28 +0200

    error fix

--- App/app.py
+++ App/app.py
@@ -337,7 +337,8 @@ with tab2:
         #Barplot
         col1.write(f"Barplot von {selected_variable} und Preis")
         fig3, ax3 = plt.subplots(figsize=(8,3.7))
-        data_price_chf = data["price"] * usd_chf #in CHF umwandeln
+        data_price_chf = data.copy()
+        data_price_chf["price"] = data["price"] * usd_chf #in CHF umwandeln
         avg_price_by_category = data_price_chf.groupby(selected_variable)["price"].mean().reset_index()
         sns.barplot(x=selected_variable, y="price", data=avg_price_by_category, ax=ax3, palette="Set2")
         plt.xticks(rotation=90)
